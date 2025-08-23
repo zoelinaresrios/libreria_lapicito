03d3fb1ab2fa7d4e9f37770d7b8ae32580683f99
zoelinaresrios <linaresrioszoe@gmail.com>
2025-08-23 00:39:11 -0300

    logout

--- admin/logout.php
+++ admin/logout.php
@@ -1,4 +1,4 @@
 <?php
 require_once __DIR__ . '/../includes/auth.php';
 session_destroy();
-header('Location: /los-lapicitos/admin/login.php');
+header('Location: /libreria_lapicito/admin/login.php');
